a33938b9ce5750bbaa5b3ce82ba84629205bba42
Mathew Webb <matsofargone@gmail.com>
2023-05-28 15:14:37 -0700

    Starting out

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,15 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             { test: /\.css$/, use: 'css-loader' },
             { test: /\.ts$/, use: 'ts-loader' },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     output: {
